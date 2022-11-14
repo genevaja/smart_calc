51863c5d53392f86d8dad8113700a1546158fef8
Albert <genevaja@student.21-school.ru>
2022-11-14 17:54:39 +0300

    Сделал файл для генерации математических выражений

--- math_fn.sh
+++ math_fn.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-declare -a lexemes=( "log" "ln" "sin" "cos" "tan" "ctg" "acos" "asin" "atan" "actg" "/" \
-    "*" "+" "-" "sqrt" "mod" "%" "^" "x" "y" "z" )
+
+declare -a trig_fn=( "log" "ln" "sin" "cos" "tan" "ctg" "acos" "asin" "atan" "actg" "sqrt" )
+
+declare -a math_fn=( "*" "-" "+" "/" "^")
+
+declare -a variables=( "x" "y" "z" "$rand_float" )
+
+declare -a rand_fn=( "trig_fn" "math_fn" "variables" )
 
 
-rand_num=0
-
-# echo ${#lexemes[@]}
-for i in {0..10}; do
-    rand_num2=$(( $RANDOM % 20))
-    for (( i=0; i < $rand_num2; i++))
-    do
-        rand_num=$(( $RANDOM % 20))
-        printf "%s(%s)" ${lexemes[$rand_num]} "$(echo $(( RANDOM % 10)).$((RANDOM%999)))"
+for lines in {0..100}; do
+    memb_of_expr=$(( $RANDOM % 20 + 1))
+    for (( j=0; j<$memb_of_expr; j++ )); do
+    rand_float="$(echo $(( $RANDOM % 10 )).$(( $RANDOM % 999 )))"
+    rand_int="$(echo $(( $RANDOM % 2000)))"
+        if [[ $memb_of_expr -eq 1 ]]; then 
+            printf "%s(%s)" "${trig_fn[$RANDOM % ${#trig_fn[@]}]}" "$rand_float"
+        elif [[ $memb_of_expr -eq 3 ]]; then
+            printf "%s%s%s" $rand_int "%" $rand_int
+            break
+        else
+            if [[ $(($j % 2)) -eq 0 ]]; then
+                printf "%s(%s)" "${trig_fn[$RANDOM % ${#trig_fn[@]}]}" "$rand_float"
+            elif [[ $(($j % 4)) -eq 3 ]]; then
+                printf "%s((%s %s %s)%s(%s%s%s))" "${trig_fn[$RANDOM % ${#trig_fn[@]}]}" "$rand_float" "${math_fn[$RANDOM % ${#math_fn[@]}]}" "$rand_float" "${math_fn[$RANDOM % ${#math_fn[@]}]}" "${variables[$RANDOM % ${#variables[@]}]}" "${math_fn[$RANDOM % ${#math_fn[@]}]}" "$rand_float"
+            else
+                printf "%s" "${math_fn[$RANDOM % ${#math_fn[@]}]}"
+                if [[ $(($j + 1)) -eq $memb_of_expr ]]; then
+                    printf "%s" $rand_float
+                fi
+            fi
+        fi 
     done
-    echo $rand_num
+    echo
 done
